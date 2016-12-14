2eb99c673d0a8512447b15d8557f0fb13d1e34d0
itggot-bjorn-karlsson <bjorn.karlsson@itggot.se>
2016-12-14 11:58:04 +0100

    Add files via upload

--- test.rb
+++ test.rb
@@ -1,1 +1,31 @@
-lol = kek
+class Deck
+  @@card = 52
+
+  def colour
+    @hearts = 1
+    @diamonds = 2
+    @spades = 3
+    @clubs = 4
+  end
+
+  def value
+    # low_ace = 1
+    # two = 2
+    # three = 3
+    # four = 4
+    # five = 5
+    # six = 6
+    # seven = 7
+    # eight = 8
+    # nine = 9
+    # ten = 10
+    # jack = 11
+    # queen = 12
+    # king = 13
+    # ace = 14
+  end
+
+  def initalize(colour, value)
+    @@card_colour
+    @@card_value
+  end
