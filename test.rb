d31a03aa1c2cb275e67ecf23ed29e72dfe4cb1e1
itggot-otto-renman <otto.renman@itggot.se>
2016-12-16 17:01:42 +0100

    Började göra rätt
    Gjorde om allt från scartch

--- test.rb
+++ test.rb
@@ -1,44 +1,37 @@
-class Deck
-  @@card1 = 13
-  @@card2 = 13
-  @@card3 = 13
-  @@card4 = 13
 
-  def colour
-    @hearts = "H"
-    @diamonds = "D"
-    @spades = "S"
-    @clubs = "C"
+class Card
+  attr_reader :value, :suite
+  def initalize(value, suite)
+    @value = value
+    @suite = suite
   end
-
-  def value
-    two = 2
-    three = 3
-    four = 4
-    five = 5
-    six = 6
-    seven = 7
-    eight = 8
-    nine = 9
-    ten = 10
-    jack = 11
-    queen = 12
-    king = 13
-    ace = [1,14]
-    list = [two,three,four,five,six,seven,eight,nine,ten,jack,queen,king,ace]
-  end
-
-  def initalize(colour, value)
-    @@card_colour
-    @@card_value
-    @card_hand1 = []
-    list.each do |x|
-     @card_hand1 = @@card1 + @hearts + x
-    end
-    return card_hand1
+  def to_s
+    return @value + @suite
   end
 end
 
-puts colour
-puts value
-puts initalize
+
+
+
+class Deck
+  attr_accessor :cards
+  def initalize
+    @cards = []
+  end
+
+  def generate
+    i = 0
+
+    cardvalue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11 ,12, 13 ]
+    while i < 52
+      @cards << Card.new(
+      end
+
+        )
+      i += 1
+    end
+  end
+end
+
+1%13
+1/13
