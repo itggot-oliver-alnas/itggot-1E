30cec6989e12f508f16bede399065d018777bfe5
itggot-oliver-alnas <oliver.alnas@itggot.se>
2016-12-16 14:46:01 +0100

    comments

--- test.rb
+++ test.rb
@@ -9,20 +9,20 @@ class Deck
   end
 
   def value
-    # low_ace = 1
-    # two = 2
-    # three = 3
-    # four = 4
-    # five = 5
-    # six = 6
-    # seven = 7
-    # eight = 8
-    # nine = 9
-    # ten = 10
-    # jack = 11
-    # queen = 12
-    # king = 13
-    # ace = 14
+    low_ace = 1
+    two = 2
+    three = 3
+    four = 4
+    five = 5
+    six = 6
+    seven = 7
+    eight = 8
+    nine = 9
+    ten = 10
+    jack = 11
+    queen = 12
+    king = 13
+    ace = 14
   end
 
   def initalize(colour, value)
